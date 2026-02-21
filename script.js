6242ed80ed2f1a743d028e97d35ac4b0ba7b6861
sebpersononline <sebysantos2006@gmail.com>
2026-02-21 01:18:43 -0400

    main page now

--- script.js
+++ script.js
@@ -1,53 +1,116 @@
-window.onload = function () {
-    // Jump to top on refresh
-    window.scrollTo(0, 0);
+let file;
+let projects;
+let skills;
 
+let selectedSkills = [];
+
+window.onload = function () {
     // Change message based on the time of day!
     let currentHour = new Date().getHours();
-    document.querySelector("#intro2").innerHTML 
-    = (currentHour <= 12) ? (currentHour < 6 ? "Happy Late Night" : "Good Morning")
-    : (currentHour < 18 ? "Good Afternoon" : "Good Evening");
+    document.querySelector("#timeOfDay").innerHTML 
+    = (currentHour <= 12) ? (currentHour < 6 ? "Happy Late Night!" : "Good Morning!")
+    : (currentHour < 18 ? "Good Afternoon!" : "Good Evening!");
 
-    window.addEventListener("scroll",onScroll);
+    // make AJAX call to retrieve the data
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        file = xhr.responseText;
+        projects = JSON.parse(file).projects;
+        skills = JSON.parse(file).skills;
+
+        loadProjects();
+        loadSkills();
+    }};
+    document.querySelector(".skills").addEventListener("click",clickSkill);
+    xhr.open("GET","data.json",true); xhr.send();
 }
 
-function onScroll() {
-    let tab1 = document.querySelector(".tab1");
-    let tab2 = document.querySelector(".tab2");
-    let tab3 = document.querySelector(".tab3");
-    let tab4 = document.querySelector(".tab4");
+function loadSkills() {
+    let html = ``;
+    let skillsContainer = document.querySelector(".skills");
+    for (let s = 0; s < skills.length; s++) {
+        let skill = skills[s];
+        html += `<div class="skillCard"><img class="skillImage" src="${skill.image}" alt="${skill.name}"><div class="skillName">${skill.name}</div></div>`;
+    }
+    skillsContainer.innerHTML = html;
+}
+function loadProjects() {
+    let html = ``;
+    let projectsContainer = document.querySelector(".projects");
+    console.log(selectedSkills);
 
-    // Divs relative to document (viewport offset + window offset)
-    let jump2 = document.querySelector("#jump2").getBoundingClientRect().top + window.scrollY-600;
-    let jump3 = document.querySelector("#jump3").getBoundingClientRect().top + window.scrollY-600;
-    let jump4 = document.querySelector("#jump4").getBoundingClientRect().top + window.scrollY-600;
+    for (let i = 0; i < projects.length; i++) {
+        let p = projects[i];
+        // Filter out if does not contain any selected skills
+        if (selectedSkills.length > 0) {
+            let valid = false;
+            for (s = 0; s < p.skills.length; s++) {
+                let skillName = p.skills[s];
+                if (selectedSkills.includes(skillName)) valid = true;
+            }
+            if (!valid) { continue; }
+        }
+        
+        html += `<div class="projectCard">`;
+        html += `<div><h2>${p.name}</h2>${p.description}`;
+        // Links
+        html += `<div class="projectLinks">`;
+        if (p.downloadLink !== undefined) { html += `<a href="${p.downloadLink}">Download Page</a>`; }
+        if (p.gitLink !== undefined) { html += `<a href="${p.gitLink}">Github Repository</a>`; }
+        if (p.demoLink !== undefined) { html += `<a href="${p.demoLink}">Video Demonstration</a>`; }
+        html += `</div>`;
+        // Request Required
+        if (p.requiresRequest) {
+            html += `<div class="projectLinks">`;
+            html += `<a class="hl projectLinkBlocked" href="#contact">Source Code is available on request.</a>`;
+            html += `</div>`;
+        }
 
-    let page1 = document.querySelector(".introPage");
-    let page2 = document.querySelector(".skillsPage");
-    let page3 = document.querySelector(".projectsPage");
-    let page4 = document.querySelector(".aboutmePage");
+        // Skills
+        html += `<div class="projectSkills">`;
+        for (let s = 0; s < p.skills.length; s++) {
+            let skillName = p.skills[s];
+            let skillObj = getSkillfromName(skillName);
+            if (skillObj != null) { html += `<img src="${skillObj.image}" alt="${skillObj.name}">`; }
+        }
+        html += `</div></div>`;
+        // Image
+        html += `<div>`;
+        html += `<img class="projectImage" src="${p.image[0]}" alt="${p.image[1]}">`;
+        html += `</div></div>`;
+    }
 
-    let pixels = window.scrollY;
+    // No projects found
+    if (html === ``) {
+        html += `<div class="hl noProjectsFound">No projects found...<br>I'll post more of my work here soon!</div>`;
+    }
+    projectsContainer.innerHTML = html;
+}
 
-    // tab1.innerHTML = pixels;
-    // tab2.innerHTML = jump2;
-    // tab3.innerHTML = jump3;
-    // tab4.innerHTML = jump4;
+function getSkillfromName(name) {
+    for (let s = 0; s < skills.length; s++) {
+        let skill = skills[s];
+        if (skill.name === name) { return skill; }
+    }
+}
 
-    if (pixels <= jump2) {
-        tab1.classList.add("selected");
-        page1.classList.remove("hidden");
-    } else { tab1.classList.remove("selected"); }
-    if (pixels > jump2 && pixels <= jump3) {
-        tab2.classList.add("selected");
-        page2.classList.remove("hidden");
-    } else { tab2.classList.remove("selected"); }
-    if (pixels > jump3 && pixels <= jump4) {
-        tab3.classList.add("selected");
-        page3.classList.remove("hidden");
-    } else { tab3.classList.remove("selected"); }
-    if (pixels > jump4) {
-        tab4.classList.add("selected");
-        page4.classList.remove("hidden");
-    } else { tab4.classList.remove("selected"); }
+function clickSkill(evt) {
+    let element = evt.target;
+    if (element.classList.contains("skillImage") || element.classList.contains("skillName")) { element = element.parentElement; }
+    if (element.classList.contains("skillCard")) {
+        // add selected
+        if (!element.classList.contains("selectedSkill")) {
+            element.classList.add("selectedSkill");
+            selectedSkills.push(element.querySelector(".skillName").innerHTML);
+            loadProjects();
+        }
+        // remove selected
+        else {
+            element.classList.remove("selectedSkill");
+            selectedSkills.splice(selectedSkills.indexOf(element.querySelector(".skillName").innerHTML),1);
+            loadProjects();
+        }
+    }
+    
 }
---
+++ unused/script.js
@@ -0,0 +1,53 @@
+window.onload = function () {
+    // Jump to top on refresh
+    window.scrollTo(0, 0);
+
+    // Change message based on the time of day!
+    let currentHour = new Date().getHours();
+    document.querySelector("#intro2").innerHTML 
+    = (currentHour <= 12) ? (currentHour < 6 ? "Happy Late Night" : "Good Morning")
+    : (currentHour < 18 ? "Good Afternoon" : "Good Evening");
+
+    window.addEventListener("scroll",onScroll);
+}
+
+function onScroll() {
+    let tab1 = document.querySelector(".tab1");
+    let tab2 = document.querySelector(".tab2");
+    let tab3 = document.querySelector(".tab3");
+    let tab4 = document.querySelector(".tab4");
+
+    // Divs relative to document (viewport offset + window offset)
+    let jump2 = document.querySelector("#jump2").getBoundingClientRect().top + window.scrollY-600;
+    let jump3 = document.querySelector("#jump3").getBoundingClientRect().top + window.scrollY-600;
+    let jump4 = document.querySelector("#jump4").getBoundingClientRect().top + window.scrollY-600;
+
+    let page1 = document.querySelector(".introPage");
+    let page2 = document.querySelector(".skillsPage");
+    let page3 = document.querySelector(".projectsPage");
+    let page4 = document.querySelector(".aboutmePage");
+
+    let pixels = window.scrollY;
+
+    // tab1.innerHTML = pixels;
+    // tab2.innerHTML = jump2;
+    // tab3.innerHTML = jump3;
+    // tab4.innerHTML = jump4;
+
+    if (pixels <= jump2) {
+        tab1.classList.add("selected");
+        page1.classList.remove("hidden");
+    } else { tab1.classList.remove("selected"); }
+    if (pixels > jump2 && pixels <= jump3) {
+        tab2.classList.add("selected");
+        page2.classList.remove("hidden");
+    } else { tab2.classList.remove("selected"); }
+    if (pixels > jump3 && pixels <= jump4) {
+        tab3.classList.add("selected");
+        page3.classList.remove("hidden");
+    } else { tab3.classList.remove("selected"); }
+    if (pixels > jump4) {
+        tab4.classList.add("selected");
+        page4.classList.remove("hidden");
+    } else { tab4.classList.remove("selected"); }
+}
